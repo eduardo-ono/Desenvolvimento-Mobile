24d10bfa08e5dcba2d58f38f4a817ef2c1a8ef8f
Eduardo Ono <eduardo.ono@gmail.com>
2020-11-18 19:58:34 -0300

    aulas/aula_05/ atualizado

--- aulas/aula_05/lib/main.dart
+++ aulas/aula_05/lib/main.dart
@@ -26,7 +26,14 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final List<Paciente> lista = [];
+
+  // Construtor
+  MyApp() {
+    Paciente exemplo = Paciente(68.0, 1.70, "Fulano de Tal");
+    lista.add(exemplo);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -35,73 +42,30 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: "Calculadora de IMC"),
+      home: HomePage(lista),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-  // Fields in a Widget subclass are always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  // Atributos
-  List<Paciente> lista = [];
-  String _nome = "";
-  double _peso = 0.0;
-  double _altura = 0.0;
-  double _imc = 0.0;
-  double _fontSize = 20.0;
-  int _index = 0;
-  String _display = "";
-
-  final nomeController = TextEditingController();
-  final pesoController = TextEditingController();
-  final alturaController = TextEditingController();
+class HomePage extends StatefulWidget {
+  final List<Paciente> lista;
 
   // Construtor
-  _MyHomePageState();
+  HomePage(this.lista);
+
+  @override
+  _HomePageState createState() => _HomePageState(lista);
+}
+
+class _HomePageState extends State<HomePage> {
+  final List<Paciente> lista;
+
+  // Construtor
+  _HomePageState(this.lista);
 
   // Métodos
-  void _cadastrarPaciente() {
-    _imc = 0.0;
-    _nome = nomeController.text;
-    _peso = double.parse(pesoController.text);
-    _altura = double.parse(alturaController.text);
-    if (_peso > 0 && _altura > 0) {
-      var paciente = Paciente(_peso, _altura, _nome);
-      _imc = paciente._imc;
-      lista.add(paciente);
-      _index = lista.length - 1;
-      nomeController.text = "";
-      pesoController.text = "";
-      alturaController.text = "";
-      _exibirRegistro(_index);
-    }
-  }
-
-  void _exibirRegistro(int index) {
-    if (index >= 0 && index < lista.length) {
-      setState(() {
-        _index = index;
-        _nome = lista[index]._nome;
-        _peso = lista[index]._peso;
-        _imc = lista[index]._imc;
-        _display = "Nome: $_nome \nPeso: ${_peso.toStringAsFixed(1)} \n"
-            "Altura: ${_altura.toStringAsFixed(2)} \n"
-            "IMC: ${_imc.toStringAsFixed(1)}";
-      });
-    } // if
+  void _atualizarTela() {
+    setState(() {});
   }
 
   @override
@@ -111,118 +75,20 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       drawer: NavDrawer(lista),
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text("Pacientes (${lista.length})"),
       ),
-      body: Container(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          // mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Padding(
-              padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-              child: Text(
-                "Dados do Paciente:",
-                style: TextStyle(fontSize: _fontSize),
-              ),
-            ),
-            // --- Nome do Paciente ---
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Nome completo",
-                  // hintText: "Nome do paciente",
-                ),
-                style: TextStyle(fontSize: _fontSize),
-                controller: nomeController,
-              ),
-            ),
-            // --- Peso do Paciente ---
-            Padding(
-              padding: EdgeInsets.all(5),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Peso (kg)",
-                  // hintText: 'Peso do paciente (kg)',
-                ),
-                style: TextStyle(fontSize: _fontSize),
-                controller: pesoController,
-              ),
-            ),
-            // --- Altura do Paciente ---
-            Padding(
-              padding: EdgeInsets.fromLTRB(5, 5, 5, 20),
-              child: TextField(
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: "Altura (m)",
-                  hintText: "Altura (m)",
-                ),
-                style: TextStyle(fontSize: _fontSize),
-                controller: alturaController,
-              ),
-            ),
-            // Saída
-            RaisedButton(
-              child: Text(
-                "Cadastrar Paciente",
-                style: TextStyle(fontSize: _fontSize),
-              ),
-              onPressed: _cadastrarPaciente,
-            ),
-            Container(
-              margin: EdgeInsets.fromLTRB(20, 20, 20, 30),
-              padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
-              color: Colors.yellow,
-              child: Text(
-                _display,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: _fontSize,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FloatingActionButton(
-            heroTag: "btn1",
-            onPressed: () => _exibirRegistro(0),
-            tooltip: 'Primeiro',
-            child: Icon(Icons.first_page),
-          ),
-          Padding(
-            padding: EdgeInsets.all(5),
-            child: FloatingActionButton(
-              heroTag: "btn2",
-              onPressed: () => _exibirRegistro(_index - 1),
-              tooltip: 'Anterior',
-              child: Icon(Icons.navigate_before),
-            ),
-          ),
-          Text("${_index + 1} / ${lista.length}"),
-          FloatingActionButton(
-            heroTag: "btn3",
-            onPressed: () => _exibirRegistro(_index + 1),
-            tooltip: 'Próximo',
-            child: Icon(Icons.navigate_next),
-          ),
-          FloatingActionButton(
-            heroTag: "btn4",
-            onPressed: () => _exibirRegistro(lista.length - 1),
-            tooltip: 'Último',
-            child: Icon(Icons.last_page),
-          ),
-        ],
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      body: ListView.builder(
+          itemCount: lista.length,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text("${lista[index]._nome}"),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _atualizarTela,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma ma
     );
   }
 }
@@ -439,7 +305,6 @@ class _TelaCadastrarPacienteState extends State<TelaCadastrarPaciente> {
   String _nome = "";
   double _peso = 0.0;
   double _altura = 0.0;
-  double _imc = 0.0;
   double _fontSize = 20.0;
   final nomeController = TextEditingController();
   final pesoController = TextEditingController();
@@ -451,7 +316,6 @@ class _TelaCadastrarPacienteState extends State<TelaCadastrarPaciente> {
 
   // Métodos
   void _cadastrarPaciente() {
-    _imc = 0.0;
     _nome = nomeController.text;
     _peso = double.parse(pesoController.text);
     _altura = double.parse(alturaController.text);
