49c5696b5cfda54d789e07826d16a4f461fa964c
Eduardo Ono <eduardo.ono@gmail.com>
2021-10-18 01:58:03 -0300

    conteudo/ atualizado

--- aulas/aula_00/exemplos/exemplo-00.dart
+++ aulas/aula_00/exemplos/exemplo-00.dart
@@ -1,1 +1,40 @@
+class Paciente {
+  // Atributos
+  String nome = '';
+  double peso = 0;
+  double altura = 0;
+  double imc = 0;
 
+  // Construtor
+  Paciente({this.nome = '', this.peso = 0, this.altura = 0}) {
+    calcularImc();
+  }
+
+  // Métodos
+  void calcularImc() {
+    if (peso > 0 && altura > 0) imc = peso / (altura * altura);
+  }
+
+  void imprimirDados() {
+    print('Nome: $nome');
+    print('Peso: $peso kg');
+    print('Altura: $altura m');
+    print('IMC: ${imc.toStringAsFixed(1)}');
+    print('');
+  }
+} // Paciente
+
+void main() {
+  Paciente paciente =
+      new Paciente(peso: 75, altura: 1.82, nome: "Fulano de Tal"); // metros
+  var paciente2 = Paciente(nome: "Ciclano de Tal", altura: 1.71, peso: 68);
+  paciente.imprimirDados();
+} // main()
+
+Paciente inserirPaciente() {
+  return Paciente(
+    nome: 'Ciclano de Tal',
+    peso: 79,
+    altura: 1.74,
+  );
+}
---
+++ conteudo/02-ambiente-de-desenvolvimento/softwares/README.md
@@ -0,0 +1,4 @@
+
+
+* https://eduardo-ono.github.io/Desenvolvimento-Mobile/conteudo/02-ambiente-de-desenvolvimento/softwares/
+<br>
